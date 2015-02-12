d6d62351b6ecc08cf30c37a5a1dd109b853e3166
Mark Lippert <mark@foresitegrp.com>
2015-02-12 13:40:43 -0600

    Initial

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+function email($address, $name="") {
+  for ($i = 0; $i < strlen($address); $i++) { $email .= (rand(0, 1) == 0) ? "&#" . ord(substr($address, $i)) . ";" : substr($address, $i, 1); }
+  if ($name == "") $name = $email;
+  echo "<a href=\"&#109;&#97;&#105;&#108;&#116;&#111;&#58;$email\">$name</a>";
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Multiscore Inc of BC Canada<?php if ($PageTitle != "") echo " | " . $PageTitle; ?></title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <meta name="description" content="Please be advised that the company Multiscore Inc will steal from you!">
+    <meta name="keywords" content="saw manufacture, rip saw, Gavin McQuade, Multiscore Inc, Plaintiff, Canada, BC, Vancouver, Delta, Model MR-21 Panel Rip Saw, MC-21 Panel Crosscut Saw, MR System Panel Rip, Crosscut Saw and the CD-4 Cabinet Groover">
+
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="inc/main.css">
+
+    <script type="text/javascript" src="inc/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $("a[href^='http'], a[href$='.pdf']").not("[href*='" + window.location.host + "']").attr('target','_blank');
+      });
+    </script>
+
+    <!--[if lt IE 9]><script src="inc/modernizr-2.6.2-respond-1.1.0.min.js"></script><![endif]-->
+  </head>
+  <body>
+
+    <article>
+      <h1>Multiscore Inc of BC Canada</h1>
+
+      <h1 style="text-align: center; font-size: 1.8em; color: #FF0000;">Warning! Warning! Warning!</h1>
+
+      <em style="color: pink; text-shadow: -1px -1px 0 #666, 1px -1px 0 #666, -1px 1px 0 #666, 1px 1px 0 #666;">"Gavin McQuade my bitch. He is a little pussyman with no dick and no balls. He will take your money and you won't get your equipment. He is a common thief!"</em><br> <br>
+
+      Please be advised that the company Multiscore Inc will steal from you!<br>
+      <br>
+
+      Multiscore Inc. located in Delta BC, Canada owned and operated by Gavin McQuade, has taken money and has not delivered the finished machine as contracted.<br>
+      <br>
+
+      Note Multiscore Inc. has recently been sued by several other companies who have experienced the same problems with Gavin McQuade and his theft.<br>
+      <br>
+
+      As published in Vancouver Business (Online Business Magazine)<br>
+      <br>
+
+      <div style="border: 1px solid #000000; background-color: #76C2E8; padding: 15px 25px; font-size: 80%; line-height: 1.2em;">
+        Tue Jan 31, 2012 12:01am PST<br>
+        <br>
+
+        Defendant: Multiscore Inc. 7157 Honeyman St., Delta<br>
+        <br>
+
+        Plaintiff: Northern Prime Wood Products Inc. dba Northern Wide Plank Floors 5930 Lloydtown Aurora Rd., Schomberg, ON<br>
+        <br>
+
+        Claim: $68,756 for the deposit and plywood processing costs, arising from the defendant's breach of a contract to sell a saw. Plaintiff: The Bank of Nova Scotia 2900-550 Burrard St., Vancouver<br>
+        <br>
+
+        Source <a href="http://www.biv.com">http://www.biv.com</a>
+      </div>
+
+      <br>
+
+      <div style="border: 1px solid #000000; background-color: #76C2E8; padding: 15px 25px; font-size: 80%; line-height: 1.2em;">
+        Tue Jul 9, 2013 12:01am PST<br>
+        <br>
+
+        Defendant: Multiscore Inc. and James Gavin McQuade 22--7157 Honeyman St., Delta and 15--7157 Honeyman St., Delta<br>
+        <br>
+
+        Plaintiff: Pacific Trim of Vancouver LLC 1100-925 W. Georgia St., Vancouver<br>
+        <br>
+
+        Claim: US$40,300 for money paid for undelivered saws and compensation for having to buy additional saw panels and hire staff, to operate additional saws that would not have been required had purchased saws arrived on time.<br>
+        <br>
+
+        Source <a href="http://www.biv.com">http://www.biv.com</a>
+      </div>
+
+      <br><br>
+      
+      <img src="images/multiscore-mr-21-sm.jpg" alt="Multiscore MR-21_SM" style="float: right; margin-left: 20px;">
+
+      Multiscore Inc builds the Model MR-21 Panel Rip Saw, MC-21 Panel Crosscut Saw, MR System Panel Rip and Crosscut Saw and the CD-4 Cabinet Groover according to their website. It does not say that Multiscore Inc will sign a contract, take your money and <em>not</em> deliver.<br>
+      <br>
+
+      Currently Gavin McQuade and Multiscore Inc has decided to screw another company out of purchased equipment and services.<br>
+      <br>
+
+      We are interested in hearing your Multiscore Inc story. Please email <?php email("GavinMcQuadeSucks@multiscoreinc.com"); ?> and tell us how Gavin took your money.<br>
+      <br>
+      <br>
+
+      If you would like to contact Gavin:<br>
+
+      <div style="float: left; width: 48%;">
+        <strong>Multiscore Inc.</strong><br>
+        15-7157 Honeyman Street<br>
+        Delta,  BC<br>
+        Canada V4G 1E2
+      </div>
+
+      <div style="float: right; width: 48%; font-weight: bold;">
+        Contact: Gavin McQuade<br>
+        E-mail: <a href="mailto:gmcquade@multiscore.com">gmcquade@multiscore.com</a><br>
+        Phone: 604-946-6130<br>
+        Toll Free: 866-775-7297<br>
+        Fax: 604-946-6135
+      </div>
+
+      <div style="clear: both;"></div>
+    </article>
+
+  </body>
+</html>
